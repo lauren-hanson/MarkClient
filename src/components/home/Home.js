829f0dd725a9cb86413917d503d64bf1591b3caf
Lauren Hanson <lohanson33@gmail.com>
2023-05-26 15:16:55 -0700

    background change when scroll

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,14 @@
 // import { Footer } from "../footer/Footer"
 import "./Home.css"
+import mark from "../../images/mark.jpg"
 
 export const Home = () => {
 
     return (<div className="homePage">
-        <h1 className="header">Home</h1>
         <div className="homePageImage">
             <img 
-                alt="Image of Mark" />
+                src={mark}
+                alt="Image of Mark"/>
         </div>
         {/* <Footer /> */}
     </div>)
