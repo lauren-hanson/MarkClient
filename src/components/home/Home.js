8a44dbd5725c0e8dbe07ad254f2fffca3a9aa0c3
Lauren Hanson <lohanson33@gmail.com>
2023-06-27 11:39:22 -0700

    working slideshow with transitions & buttons

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,6 @@
 // import { Footer } from "../footer/Footer"
+import { HomeReel } from "./HomeReel"
+import { ReelData } from "./ReelData"
 import mark from "../../assets/homePageImage.jpeg"
 import "./Home.css"
 
@@ -19,16 +21,14 @@ export const Home = () => {
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta. Imperdiet proin fermentum leo vel orci porta non pulvinar. Dictum non consectetur a erat nam at lectus urna. Est velit egestas dui id ornare arcu odio.
                     </p>
                 </div>
-                <div className="secondHomePageImage">
+                {/* <div className="secondHomePageImage">
                     <img
                         src={mark}
                         alt="Image of Mark" />
 
-                </div>
-                <div className="paragraph">
-                    <p >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem et tortor consequat id porta. Imperdiet proin fermentum leo vel orci porta non pulvinar. Dictum non consectetur a erat nam at lectus urna. Est velit egestas dui id ornare arcu odio.
-                    </p>
+                </div> */}
+                <div>
+                    <HomeReel slides={ReelData}/>
                 </div>
             </div>
         </>
